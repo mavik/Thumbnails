cf07840532323296adab75ee8555210bcadc254b
Vitalii Marenkov <admin@mavik.com.ua>
2024-02-17 13:18:27 +0100

    Add class ImageProcessor

---
+++ src/ImageProcessor.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * PHP Library for replacing images in html to thumbnails.
+ *
+ * @package Mavik\Thumbnails
+ * @author Vitalii Marenkov <admin@mavik.com.ua>
+ * @copyright 2023 Vitalii Marenkov
+ * @license MIT; see LICENSE
+ */
+
+namespace Mavik\Thumbnails;
+
+use Mavik\Image\ImageImmutable;
+use Mavik\Image\ImageWithThumbnails;
+use Mavik\Thumbnails\Configuration\Thumbnails as ConfigurationThumbnails;
+
+class ImageProcessor
+{
+    /** @var ImageFactory */
+    private $imageFactory;
+
+    public function __construct(ImageFactory $imageFactory)
+    {
+        $this->imageFactory = $imageFactory;
+    }
+
+    public function replaceToThumbnail(\DOMElement $imageTag, ConfigurationThumbnails $configurationThumbnails): void
+    {
+        $imageWithThumbnails = $this->crateImageWithThumbnails(
+            $imageTag,
+            $configurationThumbnails->resizeType(),
+            $configurationThumbnails->scales()
+        );
+        $srcset = $this->createScrset($imageWithThumbnails->getThumbnails());
+        if(!empty($srcset)) {
+            $imageTag->setAttribute('srcset', implode(', ', $srcset));
+            $imageTag->setAttribute('sizes', $imageWithThumbnails->getWidth().'px');
+        }        
+        $defaultThumbnail = $this->selectDefaultThumbnail($imageWithThumbnails);
+        $imageTag->setAttribute('src', $defaultThumbnail->getUrl());
+        $imageTag->setAttribute('width', $defaultThumbnail->getWidth());
+        $imageTag->setAttribute('height', $defaultThumbnail->getHeight());        
+    }
+
+    private function crateImageWithThumbnails(\DOMElement $imageTag, string $resizeType, array $thumbnailScails): ImageWithThumbnails
+    {
+        $src = $imageTag->getAttribute('src');
+        $width = $imageTag->getAttribute('width');
+        $height = $imageTag->getAttribute('height');
+        return $this->imageFactory->create($src, $width, $height, $resizeType, $thumbnailScails);
+    }
+    
+    /**
+     * @param ImageImmutable[] $thumbnails
+     * @return string[]
+     */
+    private function createScrset(array $thumbnails): array
+    {
+        $srcset = [];
+        foreach ($thumbnails as $thumbnail) {
+            $srcset[] = $thumbnail->getUrl() . ' ' . $thumbnail->getWidth() . 'w';
+        }
+        return $srcset;
+    }
+    
+    private function selectDefaultThumbnail(ImageWithThumbnails $imageWithThumbnails): ImageImmutable
+    {
+        $thumbnails = $imageWithThumbnails->getThumbnails();
+        if (isset($thumbnails[1])) {
+            return $thumbnails[1];
+        } elseif ($currentThumbnail = current($thumbnails)) {
+            return $currentThumbnail;
+        } else {
+            return $imageWithThumbnails;
+        }
+    }
+}
