065e9442edac2f1512a6f03085d8c11be6d249bf
Vitalii Marenkov <admin@mavik.com.ua>
2024-04-20 19:14:55 +0200

    Don't test Image in DocumentTest

--- tests/Unit/Html/DocumentTest.php
+++ tests/Unit/Html/DocumentTest.php
@@ -15,51 +15,69 @@ use PHPUnit\Framework\TestCase;
 class DocumentTest extends TestCase
 {    
     /**
-     * @covers HtmlDocument::findImages
-     * @dataProvider htmls
+     * @covers Document::findImages
+     * @dataProvider dataProvider
      */
     public function testFindImages(string $html, array $images)
     {
-        $document = Document::createFragment($html);        
+        $document = Document::createFragment($html);
+        $dom = new \DOMDocument();
         foreach ($document->findImages() as $index => $image) {
-            $this->assertInstanceOf(Image::class, $image);
-            $this->assertEquals($images[$index]['src'], $image->getSrc());
-            $this->assertEquals($images[$index]['width'], $image->getWidth());
-            $this->assertEquals($images[$index]['heigth'], $image->getHeight());
-            $this->assertEquals($images[$index]['inPixels'], $image->isInPixels());
-            $this->assertEquals($images[$index]['inStyle'], $image->inStyle());
+            /** @var Image $image */
+            $imgTag = $dom->createElement('img');
+            $imgTag->setAttribute('src', $images[$index]['src']);
+            $imgTag->setAttribute('width', $images[$index]['width']);
+            $imgTag->setAttribute('height', $images[$index]['height']);
+            $imgTag->setAttribute('style', $images[$index]['style']);
+            $testImage = new Image($imgTag);       
+            $this->assertEquals($testImage->getAttribute('src'), $image->getAttribute('src'));
+            $this->assertEquals($testImage->getAttribute('width'), $image->getAttribute('width'));
+            $this->assertEquals($testImage->getAttribute('height'), $image->getAttribute('height'));
+            $this->assertEquals($testImage->getAttribute('style'), $image->getAttribute('style'));
         }
     }
     
-    public function htmls(): array
+    public function dataProvider(): array
     {
         return [
             0 => [
-                'START adsf <img src="test0" width="10" height="20"> fg sdg
+                'START adsf <img src="test0" width="10"
+                height="20"> fg sdg
                 asfsf asf <div> <img width="20px" height="30 px" src="test1" />
-                <p>adssdf sadf <img width="20px" height="30 px" src="test2" style="width: 40px; height: 50 px" />
+                <p>adssdf sadf <img width="20px" height="30 px" src="test2" style="width: 40px; height: 50 px">
                 <style>font-size: 12px</style>
+                <img width="30px" src="test3" style="height: 50 px"></img>
+                <img height="30px" src="test4" style="width: 50px"></img>
                 <script>alert("TEST")</script>
                 FINISH',
                 [
-                    'src' => 'test0',
-                    'width' => 10,
-                    'height'=> 20,
-                    'inPixels' => true,
-                    'inStyle' => false,
-                ], [
-                    'src' => 'test1',
-                    'width' => 20,
-                    'height'=> 30,
-                    'inPixels' => true,
-                    'inStyle' => false,
-                ], [
-                    'src' => 'test2',
-                    'width' => 40,
-                    'height'=> 50,
-                    'inPixels' => true,
-                    'inStyle' => true,
-                ]
+                    [
+                        'src' => 'test0',
+                        'width' => '10',
+                        'height'=> '20',
+                        'style' => null,
+                    ], [
+                        'src' => 'test1',
+                        'width' => '20px',
+                        'height'=> '30 px',
+                        'style' => null,
+                    ], [
+                        'src' => 'test2',
+                        'width' => '20px',
+                        'height'=> '30 px',
+                        'style' => 'width: 40px; height: 50 px',
+                    ], [
+                        'src' => 'test3',
+                        'width' => '30px',
+                        'height'=> null,
+                        'style' => 'height: 50 px',
+                    ], [
+                        'src' => 'test4',
+                        'width' => null,
+                        'height'=> '30px',
+                        'style' => 'width: 50px',
+                    ],
+                ],
             ],
         ];
     }
